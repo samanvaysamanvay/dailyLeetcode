c80a0dbe6936a9e3b74e2296b1dba74111feb476
samanvaysamanvay <ssrivastava_be21@thapar.edu>
2023-12-25 23:29:26 +0530

    25.12.23

---
+++ decodeWays.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+        int Choices(string s, int index, vector<int> &dp)
+        {
+            if (index == s.size())
+                return 1;
+            if (dp[index] != -1)
+                return dp[index];
+            int res = 0;
+            if (s[index] != '0')
+                res += Choices(s, index + 1, dp);
+            if (index + 1 < s.size() && (s[index] == '1' || s[index] == '2' && s[index + 1] <= '6'))
+                res += Choices(s, index + 2, dp);
+            return dp[index] = res;
+        }
+    int numDecodings(string s)
+    {
+        int n = s.size();
+        vector<int> dp(n + 1, -1);
+        return Choices(s, 0, dp);
+    }
+
+};
