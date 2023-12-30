b835c4228bbe0698dd0e7732915a0273e595d67f
samanvaysamanvay <ssrivastava_be21@thapar.edu>
2023-12-30 06:38:56 +0530

    30.12.23

---
+++ redistributeCharactersToMakeAllStringsEqual.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool makeEqual(vector<string>& words) {
+         unordered_map<char,int> mp;
+         for(int i=0;i<words.size();i++){
+             for(int j=0;j<words[i].size();j++){
+                 mp[words[i][j]]++;
+             }
+         }
+         int n=words.size();
+         for(auto i:mp){
+             if(i.second%n!=0) return false;
+         }
+         return true;
+    }
+};
