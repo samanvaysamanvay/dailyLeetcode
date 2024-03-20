4865410024196ba0b4dc10e15c9752223a81315e
samanvaysamanvay <ssrivastava_be21@thapar.edu>
2024-03-20 17:32:19 +0530

    20.03.24

---
+++ mergeInBetweenLinkedList.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    ListNode* mergeInBetween(ListNode* list1, int a, int b, ListNode* list2) {
+        a--, b++;
+        ListNode *l1 = list1;
+        ListNode *l2 = l1;
+        while(a>0 || b>0) {
+            if(a>0) {
+                l1 = l1->next;
+                a--;
+            }
+            if(b>0) {
+                l2 = l2->next;
+                b--;
+            }
+        }
+        l1->next = list2;
+        while(l1->next != NULL) {
+            l1 = l1->next;
+        }
+        l1->next = l2;
+        return list1;
+    }
+};
