c6809d92d795650a613e886c29c951f0c3b6ee3b
samanvaysamanvay <ssrivastava_be21@thapar.edu>
2024-07-09 13:03:54 +0530

    08.07.24

---
+++ averageWaitingTime.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    double averageWaitingTime(vector<vector<int>>& customers) {
+        double ans=0;
+        int prev=0;
+        for(int i=0;i<customers.size();i++){
+            if(prev>customers[i][0]) ans+=(prev-customers[i][0])+customers[i][1];
+            else ans+=customers[i][1];
+            prev=max(prev,customers[i][0])+customers[i][1];
+        }
+        return ans/customers.size();
+    }
+};
